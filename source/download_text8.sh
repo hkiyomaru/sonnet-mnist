2ef249290fcf9c1de7de5ecd8f4c6c8297fb8e73
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2017-04-16 15:13:23 +0900

    remove download shell script.

--- source/download_text8.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo 'wget http://mattmahoney.net/dc/text8.zip -O ../data/text8.zip'
-wget http://mattmahoney.net/dc/text8.zip -O ../data/text8.zip
-echo 'unzip ../data/text8.zip'
-unzip ../data/text8.zip -d ../data/
