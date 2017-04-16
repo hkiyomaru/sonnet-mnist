2a2f196b306a1569b70354866d832bfbb964fff8
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2017-04-16 14:40:31 +0900

    clean-up training script

--- source/train_w2v.py
+++ source/train_w2v.py
@@ -1,95 +1,90 @@
-# Inspired by https://www.tensorflow.org/versions/r0.7/tutorials/word2vec/index.html
+"""Sonnet implementation of word2vec."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import collections
-import matplotlib.pyplot as plt
-import numpy as np
-import tensorflow as tf
+import os
+from six.moves import urllib
 import zipfile
 
+import numpy as np
+import tensorflow as tf
+
 # Configuration
-batch_size = 20
-# Dimension of the embedding vector. Two too small to get
-# any meaningful embeddings, but let's make it 2 for simple visualization
+batch_size = 128
 embedding_size = 2
 num_sampled = 15  # Number of negative examples to sample.
+vocab_size = 30000
 
 
-# Read the data into a list of strings.
+def download(filename='text8.zip', url='http://mattmahoney.net/dc/'):
+    """Download a file if it does not exist."""
+    if os.path.exists(filename):
+        print("{} already exists.".format(filename))
+    else:
+        print('Start downloading {}.'.format(filename))
+        filename, _ = urllib.request.urlretrieve(url + filename, filename)
+    return filename
+
+
 def read_data(filename):
-    """Extract the first file enclosed in a zip file as a list of words"""
+    """Extract the corpus, which has a list of words."""
     with zipfile.ZipFile(filename) as f:
         data = tf.compat.as_str(f.read(f.namelist()[0])).split()
     return data
 
 
+def build_dataset(words, vocabulary_size=vocab_size):
+    count = [('UNK', -1)]
+    count.extend(collections.Counter(words).most_common(vocabulary_size - 1))
+    dictionary = {word: i for i, (word, _) in enumerate(count)}
+    data = [dictionary[word] if word in dictionary else 0 for word in words]
+    reverse_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+    return data, count, dictionary, reverse_dictionary
+
+
 # sentences to words and count
-words = read_data('../data/text8.zip')
-count = collections.Counter(words).most_common()
-print ("Word count", count[:5])
+filename = download()  # download text8.zip
+words = read_data(filename)
 
 # Build dictionaries
-rdic = [i[0] for i in count]  # reverse dic, idx -> word
-dic = {w: i for i, w in enumerate(rdic)}  # dic, word -> id
-voc_size = len(dic)
-
-# Make indexed word data
-data = [dic[word] for word in words]
-print('Sample data', data[:10], [rdic[t] for t in data[:10]])
+data, count, dictionary, reverse_dictionary = build_dataset(words)
 
 # Let's make a training data for window size 1 for simplicity
 # ([the, brown], quick), ([quick, fox], brown), ([brown, jumped], fox), ...
-cbow_pairs = []
-for i in range(1, len(data)-1):
-    cbow_pairs.append([[data[i-1], data[i+1]], data[i]])
-print('Context pairs', cbow_pairs[:10])
+cbow_pairs = [[[data[i-1], data[i+1]], data[i]] for i in range(len(data)-1)]
 
 # Let's make skip-gram pairs
 # (quick, the), (quick, brown), (brown, quick), (brown, fox), ...
-skip_gram_pairs = []
-for c in cbow_pairs:
-    skip_gram_pairs.append([c[1], c[0][0]])
-    skip_gram_pairs.append([c[1], c[0][1]])
-print('skip-gram pairs', skip_gram_pairs[:5])
+skip_gram_pairs = [[c[1], c[0][0]] for c in cbow_pairs] + \
+                  [[c[1], c[0][1]] for c in cbow_pairs]
 
 
 def generate_batch(size):
-    assert size < len(skip_gram_pairs)
-    x_data = []
-    y_data = []
     r = np.random.choice(range(len(skip_gram_pairs)), size, replace=False)
-    for i in r:
-        x_data.append(skip_gram_pairs[i][0])  # n dim
-        y_data.append([skip_gram_pairs[i][1]])  # n, 1 dim
-    return x_data, y_data
+    x_batch = [skip_gram_pairs[i][0] for i in r]
+    y_batch = [[skip_gram_pairs[i][1]] for i in r]
+    return x_batch, y_batch
 
 
-# generate_batch test
-print ('Batches (x, y)', generate_batch(3))
-
-# Input data
 train_inputs = tf.placeholder(tf.int32, shape=[batch_size])
-# need to shape [batch_size, 1] for nn.nce_loss
 train_labels = tf.placeholder(tf.int32, shape=[batch_size, 1])
-# Ops and variables pinned to the CPU because of missing GPU implementation
+
 with tf.device('/cpu:0'):
     # Look up embeddings for inputs.
     embeddings = tf.Variable(
-        tf.random_uniform([voc_size, embedding_size], -1.0, 1.0))
+                 tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0))
     embed = tf.nn.embedding_lookup(embeddings, train_inputs)  # lookup table
 
-# Construct the variables for the NCE loss
 nce_weights = tf.Variable(
-    tf.random_uniform([voc_size, embedding_size], -1.0, 1.0))
-nce_biases = tf.Variable(tf.zeros([voc_size]))
+    tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0))
+nce_biases = tf.Variable(tf.zeros([vocab_size]))
 
-# Compute the average NCE loss for the batch.
-# This does the magic:
-# tf.nn.nce_loss(weights, biases, inputs, labels, num_sampled, num_classes ...)
-# It automatically draws negative samples when we evaluate the loss.
 loss = tf.reduce_mean(
        tf.nn.nce_loss(nce_weights, nce_biases, train_labels,
-                      embed, num_sampled, voc_size))
+                      embed, num_sampled, vocab_size))
 
-# Use the adam optimizer
 train_op = tf.train.AdamOptimizer(1e-1).minimize(loss)
 
 # Launch the graph in a session
@@ -103,17 +98,6 @@ with tf.Session() as sess:
                                feed_dict={train_inputs: batch_inputs,
                                           train_labels: batch_labels})
         if step % 10 == 0:
-            print("Loss at ", step, loss_val)  # Report the loss
+            print("Step: {}, Loss: {}".format(step, loss_val))
 
-    # Final embeddings are ready for you to use. Need to normalize for practical use
     trained_embeddings = embeddings.eval()
-
-# Show word2vec if dim is 2
-if trained_embeddings.shape[1] == 2:
-    labels = rdic[:10]  # Show top 10 words
-    for i, label in enumerate(labels):
-        x, y = trained_embeddings[i, :]
-        plt.scatter(x, y)
-        plt.annotate(label, xy=(x, y), xytext=(5, 2),
-                     textcoords='offset points', ha='right', va='bottom')
-    plt.savefig("word2vec.png")
